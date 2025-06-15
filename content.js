0148ba8fac555998198bdbd8ae2ecf95de369df4
北﨑 碧恵 <>
2025-06-16 01:25:43 +0900

    change-2025.06.16.24 01:25:43

--- content.js
+++ content.js
@@ -7,31 +7,7 @@ function MarqueeMessage() {
     $inner.animate({'translate': -$inner.width() + 'px'}, 30000, 'linear', MarqueeMessage);
 }
 
-function InitTopics() {
-    const $topic = $('.main .topics .mask .fixed .topic');
-    $('.main .topics .mask .range').width($topic.length * 100 + '%');
-    for (let i = 0; i < $topic.length; i++) {
-        $topic.eq(i).css({'z-index': $topic.length - i});
-    }
-    UpdateTopics();
-}
-
-function UpdateTopics() {
-    const $topics = $('.main .topics .mask');
-    const unit = $topics.width();
-    const $topic = $('.main .topics .mask .fixed .topic');
-    for (let i = 0; i < $topic.length; i++) {
-        $topic.eq(i).find('.frame').css({'width': unit + 'px'});
-        let width = Math.min(unit, unit * (i + 1) - $topics.scrollLeft());
-        $topic.eq(i).css({'width': width + 'px'});
-    }
-}
-
 $(window).on('load', () => {
     MarqueeMessage();
-
-    InitTopics();
-    $('.main .topics .mask').scroll(UpdateTopics);
-    $(window).resize(UpdateTopics);
 });
 
