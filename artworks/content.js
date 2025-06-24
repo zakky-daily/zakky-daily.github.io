32d2f9fa93ff77125d805dfa396de57d69c6d93b
北﨑 碧恵 <>
2025-06-24 15:08:51 +0900

    change-2025.06.24.25 15:08:51

--- artworks/content.js
+++ artworks/content.js
@@ -1,4 +1,19 @@
-function ExpandItem(element) {
+function ExpandItem(target) {
 
-    $(element).parent().toggleClass('item-active');
+    $('.items .item').each(function(_, element) {
+
+        const $header = $(element).children('.header');
+        const $detail = $(element).children('.detail');
+
+        if (!$(element).hasClass('item-active') && $header[0] === target) {
+
+            $(element).addClass('item-active');
+            $detail.css({'height': $detail[0].scrollHeight + 'px'});
+
+        } else {
+
+            $(element).removeClass('item-active');
+            $detail.css({'height': 0});
+        }
+    });
 }
