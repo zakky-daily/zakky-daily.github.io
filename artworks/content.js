1e1ad884bc34f42608363fa8fdf59c1be6f1788e
北﨑 碧恵 <>
2025-06-30 04:19:26 +0900

    change-2025.06.30.26 04:19:26

--- artworks/content.js
+++ artworks/content.js
@@ -1,19 +1,38 @@
-function ExpandItem(target) {
+function ChangeHeight() {
+
+    $('.items .item').each(function(_, element) {
+        
+        const $detail = $(element).children('.detail');
+
+        if ($(element).hasClass('item-active')) {
+
+            $detail.css({height: 'fit-content'});
+            const height = $detail.outerHeight();
+            $detail.css({height: '0px'});
+            requestAnimationFrame(() => $detail.css({height: height + 'px'}));
+            
+        } else {
+            $detail.css({height: '0px'});
+        }
+    });
+}
+
+function ChangeActive(target) {
 
     $('.items .item').each(function(_, element) {
 
         const $header = $(element).children('.header');
-        const $detail = $(element).children('.detail');
 
         if (!$(element).hasClass('item-active') && $header[0] === target) {
-
             $(element).addClass('item-active');
-            $detail.css({'height': $detail[0].scrollHeight + 'px'});
-
         } else {
-
             $(element).removeClass('item-active');
-            $detail.css({'height': 0});
         }
     });
+    ChangeHeight();
 }
+
+$(window).resize(function() {
+
+    ChangeHeight();
+});
