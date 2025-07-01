ad13efea0329cc1637afaf6f7caaa980c4c01967
北﨑 碧恵 <>
2025-07-01 14:17:04 +0900

    change-2025.07.01.26 14:17:04

--- artworks/content.js
+++ artworks/content.js
@@ -1,38 +1,63 @@
-function ChangeHeight() {
+'use strict'
 
-    $('.items .item').each(function(_, element) {
-        
+function ChangeActiveTab(target) {
+
+    let currTabIndex = parseInt($.cookie('active-tab-index'));
+
+    if (currTabIndex == $(target).parent().index())
+        currTabIndex = -1;
+    else
+        currTabIndex = $(target).parent().index();
+
+    $.cookie('active-tab-index', currTabIndex);
+    UpdateTab();
+}
+
+
+function UpdateTab() {
+
+    let currTabIndex = $.cookie('active-tab-index');
+    if (currTabIndex === undefined) currTabIndex = -1;
+
+    $('.items .item').each(function(i, element) {
+
         const $detail = $(element).children('.detail');
 
-        if ($(element).hasClass('item-active')) {
+        if (i == currTabIndex) {
+
+            $(element).addClass('item-active');
+            $(element).removeClass('item-noactive');
 
             $detail.css({height: 'fit-content'});
             const height = $detail.outerHeight();
             $detail.css({height: '0px'});
             requestAnimationFrame(() => $detail.css({height: height + 'px'}));
-            
+        
         } else {
+
+            $(element).removeClass('item-active');
+            if (currTabIndex == -1)
+                $(element).removeClass('item-noactive');
+            else
+                $(element).addClass('item-noactive');
+
             $detail.css({height: '0px'});
         }
     });
 }
 
-function ChangeActive(target) {
-
-    $('.items .item').each(function(_, element) {
-
-        const $header = $(element).children('.header');
-
-        if (!$(element).hasClass('item-active') && $header[0] === target) {
-            $(element).addClass('item-active');
-        } else {
-            $(element).removeClass('item-active');
-        }
-    });
-    ChangeHeight();
-}
 
 $(window).resize(function() {
-
-    ChangeHeight();
+    
+    UpdateTab();
 });
+
+$(window).on('scroll', () => {
+
+    $.cookie('scroll-pos', $(window).scrollTop());
+})
+
+$(window).on('load', () => {
+
+    UpdateTab();
+})
